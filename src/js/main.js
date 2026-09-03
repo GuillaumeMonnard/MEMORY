281fc1acfd980f8809b6244ab0c4a0d2063ce1d1
monnard-guillaume <monnard.guillaume@gmail.com>
2026-09-03 15:54:46 +0200

    ajout d'une classe hidden pour tourner les cartes au début de la partie et ajout d'un écouteur d'événement pour retourner les cartes

--- src/js/main.js
+++ src/js/main.js
@@ -11,25 +11,60 @@ const emojis = [
   "🌵",
   "🌻",
   "🐝",
-  "✌️",
-  "🫥",
-  "❤️",
-  "👹",
-  "🦷",
-  "👻",
+  "👄",
+  "🧚‍♀️",
+  "💩",
+  "🐢",
+  "🤡",
+  "👁️",
+  "🐤",
+  "🙊",
+  "🌽",
+  "🌵",
+  "🌻",
+  "🐝",
 ]; //tableau des emojis
+
+//fonction de mélange aléatoire
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
 
 //récupération du board
 const board = document.querySelector("#board");
+
+//mélange des cartes
+shuffle(emojis);
 
 //boucle pour la création des cartes
 emojis.forEach((emoji) => {
   //création d'un carte
   const card = document.createElement("div");
-  //lien entre la carte et le style .card
-  card.classList.add("card");
+
+  //lien entre la carte et le style .card et .hidden
+  card.classList.add("card", "hidden");
+
   //lien entre le dataset.emoji et les attribut css
   card.dataset.emoji = emoji;
+
   //ajout de la carte au board
   board.appendChild(card);
+
+  //retourner la carte au click
+  card.addEventListener("click", function () {
+    card.classList.remove("hidden");
+  });
 });
