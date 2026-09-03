fd08d562e0eebd69eeb3080e689418e3de1b4a74
monnard-guillaume <monnard.guillaume@gmail.com>
2026-09-03 14:35:50 +0200

    création de la carte et ajout au boad et application du style

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+//récupération du board
 const board = document.querySelector("#board");
+
+//création d'un carte
+const card = document.createElement("div");
+//lien entre la carte et le style .card
+card.classList.add("card");
+
+//ajout de la carte au board
+board.appendChild(card);
