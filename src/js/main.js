51df07daa4f8ce45a4b4038f6c5b1ac9ecc16fac
monnard-guillaume <monnard.guillaume@gmail.com>
2026-09-24 16:18:43 +0200

    sortir la fonction du listener pour pouvoir utiliser le clavier

--- src/js/main.js
+++ src/js/main.js
@@ -87,56 +87,38 @@ function startGame() {
   //mélange des cartes
   shuffle(symboles);
   //boucle pour la création des cartes
-  symboles.forEach((symbole) => {
+  symboles.forEach((symbole, index) => {
     //création d'un carte
     const card = document.createElement("div");
-
     //lien entre la carte et le style .card et .hidden
     card.classList.add("card", "hidden");
-
     //lien entre le dataset.symbole et les attribut css
     card.dataset.symbole = symbole;
-
     //ajout de la carte au board
     board.appendChild(card);
-
     //retourner la carte au click
-    card.addEventListener("click", function () {
-      //condition: si firstChoice n'a pas encore de carte assignée
+    function selectCard(card) {
       if (firstChoice === null) {
-        //on attibue la carte actuelle à firstChoice
         firstChoice = card;
-        //et on la révèle
         card.classList.remove("hidden");
-        //style de carte active
         card.classList.add("active");
       } else if (secondChoice === null) {
-        //on vérifie si la carte a déjà la classe active
         if (card.classList.contains("active")) {
-          //si oui, on ne fait rien
+          // rien
         } else {
-          //si non on peut activer la deuxième carte
-          //on attribue la carte actuelle à secondChoice
           secondChoice = card;
-          //et on la révèle
           card.classList.remove("hidden");
-          //style de carte active
           card.classList.add("active");
         }
       } else {
-        //vérification des deux cartes
-        //Si les cartes ont le même symbole
         if (firstChoice.dataset.symbole === secondChoice.dataset.symbole) {
-          //on active les cartes
           firstChoice.classList.remove("active");
           secondChoice.classList.remove("active");
-          //on fait en sorte de ne plus pouvoir sélectionner ces cartes, car elles sont hors du jeu
           firstChoice.classList.add("found");
           secondChoice.classList.add("found");
           firstChoice = null;
           secondChoice = null;
           count += 1;
-          //vérifier s'il reste des cartes
           pairsFoundNumber = document.querySelectorAll(".found").length / 2;
           if (pairsFoundNumber === pairsNumber) {
             const winScreen = document.createElement("div");
@@ -150,11 +132,8 @@ function startGame() {
             restartButton.addEventListener("click", startGame);
           }
         } else {
-          //Si elles sont différentes
-          //on les caches
           firstChoice.classList.add("hidden");
           secondChoice.classList.add("hidden");
-          //on désactive les cartes
           firstChoice.classList.remove("active");
           secondChoice.classList.remove("active");
           firstChoice = null;
@@ -162,7 +141,7 @@ function startGame() {
           count += 1;
         }
       }
-    });
+    }
   });
 }
 
